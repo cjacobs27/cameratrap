91382622daf1e6eb8c0e1cba197df7cd6586bc28
Chelsey <jacobs27c@gmail.com>
2017-08-05 15:24:30 +0100

    FIX INTERMITTENT BUG
    Fingers crossed...

--- cameratrap.py
+++ cameratrap.py
@@ -10,29 +10,29 @@ import cv2, time, pandas, os
 from datetime import datetime
 
 first_frame=None
-mstatus_list=[None,None]
-mtimes=[]
+# mstatus_list=[None,None]
+# mtimes=[]
+# mdf=pandas.DataFrame(columns=["Start","End"])
 fstatus_list=[None,None]
 ftimes=[]
-mdf=pandas.DataFrame(columns=["Start","End"])
 fdf=pandas.DataFrame(columns=["Start","End"])
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 video=cv2.VideoCapture(0)
 
-def takepic(fstatus):
-    if fstatus == 1:
-        file = r"C:\Users\Chelsey\Documents\Projects\cameratrap\cameratrap_faces\\"
-        if os.path.exists(file):
-            now = datetime.now().strftime("%I.%M.%S.%f")
-            cv2.imwrite(file + "_captured_" + now + ".jpg", frame)
-            print("Saving image.")
-            time.sleep(1)
-    elif fstatus == 0:
-        pass
+# def takepic(fstatus):
+#     if fstatus == 1:
+#         file = r"C:\Users\Chelsey\Documents\Projects\cameratrap\cameratrap_faces\\"
+#         if os.path.exists(file):
+#             now = datetime.now().strftime("%I.%M.%S.%f")
+#             cv2.imwrite(file + "_captured_" + now + ".jpg", frame)
+#             print("Saving image.")
+#             time.sleep(1)
+#     elif fstatus == 0:
+#         pass
 
 while True:
     check, frame = video.read()
-    mstatus=0
+    # mstatus=0
     fstatus=0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray,(21,21),0)
@@ -54,12 +54,10 @@ while True:
     for contour in cnts:
         if cv2.contourArea(contour) < 10000:
             continue
-
-        mstatus=1
+        # mstatus=1
 
         (x, y, w, h)=cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 1)
-
     # mstatus_list.append(mstatus)
 
     for x, y, w, h in faces:
@@ -70,6 +68,7 @@ while True:
         # takepic(fstatus)
 
     fstatus_list.append(fstatus)
+    fstatus_list=fstatus_list[-2:]
 
     # if mstatus_list[-1]==1 and mstatus_list[-2]==0:
     #     mtimes.append(datetime.now())
@@ -81,6 +80,7 @@ while True:
     if fstatus_list[-1]==0 and fstatus_list[-2]==1:
         ftimes.append(datetime.now())
 
+    print(fstatus_list)
     # cv2.imshow("Gray Frame",gray)
     # cv2.imshow("Delta Frame",delta_frame)
     # cv2.imshow("Threshold Frame",thresh_frame)
@@ -89,16 +89,18 @@ while True:
     key=cv2.waitKey(1)
 
     if key==ord('q'):
-        if fstatus==1:
-            ftimes.append(datetime.now())
-        if mstatus==1:
-            mtimes.append(datetime.now())
+        ftimes.append(datetime.now())
         break
+print(fstatus_list)
+print(ftimes)
 
 for f in range(0,len(ftimes),2):
-    fdf=fdf.append({"Start":ftimes[f],"End":ftimes[f+1]},ignore_index=True)
-
-
+    if len(ftimes) % 2 == 0:
+        fdf=fdf.append({"Start":ftimes[f],"End":ftimes[f+1]}, ignore_index=True)
+    else:
+        ftimes.append(datetime.now())
+        fdf = fdf.append({"Start": ftimes[f], "End": ftimes[f + 1]}, ignore_index=True)
+print(fdf)
 # for m in range(0,len(mtimes),2):
 #     mdf=mdf.append({"Start":mtimes[m],"End":mtimes[m+1]},ignore_index=True)
 
