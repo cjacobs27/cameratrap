9556389ee7d436a7e6a706557400a146eea459cc
Chelsey <jacobs27c@gmail.com>
2017-08-05 16:15:15 +0100

    Format fdf glyph & graph

--- trap_plotting.py
+++ trap_plotting.py
@@ -2,7 +2,12 @@ from cameratrap import fdf
 from bokeh.plotting import figure, show, output_file
 
 f = figure(x_axis_type='datetime',height=100,width=500,responsive=True,title="Face Detection Events")
+f.title.align='center'
+f.title.text_font_size='12pt'
+f.title.text_font_style='bold'
+f.title.text_font='helvetica'
 f.yaxis.minor_tick_line_color=None
+f.ygrid[0].ticker.desired_num_ticks=1
 q = f.quad(left=fdf["Start"],right=fdf["End"],bottom=0,top=1,color="purple")
 
 output_file("FaceGraph.html")
